30635ccc5722a8db84411e02a0e559dc1a738e95
AyozeAlmeida <ayozeypunto@hotmail.com>
2017-06-05 13:36:58 +0100

    CAMBIO EN EL LOG

--- app/src/main/java/com/example/funiculi/trabajo/CargadorJSON.java
+++ app/src/main/java/com/example/funiculi/trabajo/CargadorJSON.java
@@ -30,7 +30,7 @@ public class CargadorJSON {
             InputStream is = conexion.getInputStream();
 
             InputStreamReader isr = new InputStreamReader(is);
-            Log.v("coñoo","llego22");
+            Log.v("CAMBIO NUMERO 1","llego22");
             BufferedReader br = new BufferedReader(isr);
             StringBuilder respuesta = new StringBuilder();
             String datos;
